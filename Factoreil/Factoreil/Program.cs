19517a9228590c3c459212363093934150a5d3ee
Alibozorgani1382890 <149718445+Alibozorgani1382890@users.noreply.github.com>
2023-12-05 05:15:25 -0800

    Add files via upload

---
+++ Factoreil/Factoreil/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factoreil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            long fact = 1;
+            Console.WriteLine("yek adad baraye mohsebe factoril vared konid");
+            n  =int.Parse(Console.ReadLine());
+            for (i = n; i >= 1; i--)
+            {
+                fact *= i;    ///    fact=fast*i;   120
+            }
+            Console.WriteLine("factoril = " + fact);
+            Console.ReadKey();
+        }
+    }
+}
